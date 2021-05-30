316467097d4bcca807ccbe137da51f9dee024c5f
daniml3 <danimoral1001@gmail.com>
2021-05-30 21:39:17 +0200

    sdm: core: allow to skip refresh rate changes
    * This function makes the screen change the refresh rate constantly
      in some situations, and this produces flicker on the display
    
    * Devices may opt-out of this function by setting the property
      vendor.display.skip_refresh_rate_change to 1
    
    * The refresh rate is still successfully changed even with this
      function disabled
    
    test: set the property vendor.display.skip_refresh_rate_change to 1,
          ensure that no flicker is visible, that the panel refresh
          rate isn't randomly changed, and that the refresh rate
          can be manually changed
    
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I613d04da10daeb7d7963d3c5d4816d3d260c98c7

--- include/display_properties.h
+++ include/display_properties.h
@@ -136,4 +136,6 @@
 #define SKIP_REFRESH_RATE_CHANGE             DISPLAY_PROP("skip_refresh_rate_change")
 #define OVERRIDE_DOZE_MODE_PROP              DISPLAY_PROP("override_doze_mode")
 
+#define SKIP_REFRESH_RATE_CHANGE             DISPLAY_PROP("skip_refresh_rate_change")
+
 #endif  // __DISPLAY_PROPERTIES_H__
